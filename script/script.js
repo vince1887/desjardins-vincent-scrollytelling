b3dd54d6b02894b788f49a295ea30bcf32c5af21
vince1887 <vincentdesjardins1887@gmail.com>
2023-05-12 01:12:24 -0400

    dvssdvsdv

--- script/script.js
+++ script/script.js
@@ -28,18 +28,61 @@ function Scrolling() {
 window.addEventListener("scroll", Scrolling);
 
 //animation-chapitre 1
+
 let chapitre1 = document.querySelector("div#voiture");
 gsap.to(chapitre1, {
-  x: "100%",
   scrollTrigger: {
     pin: true,
-    pinSpacing: false,
     scrub: true,
-    markers: true,
-    start: "top 25%",
-    end: "bottom 75%",
-    trigger: ".car",
+
+    start: "top top",
+    end: "bottom 40%",
+    trigger: chapitre1,
   },
+});
+
+//animation voiture
+
+gsap.to(".car", {
+  scrollTrigger: {
+    trigger: ".car",
+    start: "50% 75%",
+    end: "50% 25%",
+    scrub: 1,
+  },
+  x: "50vw",
+  duration: 2,
+});
+
+//animation-chapitre 2
+
+let chapitre2 = document.querySelector("div#mongolfiert");
+gsap.to(chapitre2, {
+  scrollTrigger: {
+    pin: true,
+    scrub: true,
+    start: "top top",
+    end: "bottom 40%",
+    trigger: chapitre2,
+  },
+});
+
+//animation mongolfiert
+
+gsap.to(".m1", {
+  motionPath: {
+    path: "#path",
+    align: "#path",
+    alignOrigin: [0.5, 0.5],
+  },
+  scrollTrigger: {
+    trigger: ".m1",
+    start: "50% 75%",
+    end: "50% 25%",
+    scrub: 1,
+  },
+  x: "50vw",
+  duration: 2,
 });
 
 //animation nuage-1
@@ -94,6 +137,62 @@ gsap.from(".nuage-3", {
   duration: 5,
   ease: "circ",
   yoyo: true,
+});
+
+//animation-chapitre 3
+
+let chapitre3 = document.querySelector("div#mongolfiert-plui");
+gsap.to(chapitre2, {
+  scrollTrigger: {
+    pin: true,
+    scrub: true,
+    markers: true,
+    start: "top top",
+    end: "bottom 40%",
+    trigger: chapitre3,
+  },
+});
+
+//animation-chapitre 4
+
+let chapitre4 = document.querySelector("div#bateau");
+gsap.to(chapitre2, {
+  scrollTrigger: {
+    pin: true,
+    scrub: true,
+    markers: true,
+    start: "top top",
+    end: "bottom 40%",
+    trigger: chapitre4,
+  },
+});
+
+//animation-chapitre 5
+
+let chapitre5 = document.querySelector("div#sous-marin");
+gsap.to(chapitre2, {
+  scrollTrigger: {
+    pin: true,
+    scrub: true,
+    markers: true,
+    start: "top top",
+    end: "bottom 40%",
+    trigger: chapitre5,
+  },
+});
+
+//animation-chapitre 6
+
+let chapitre6 = document.querySelector("div#fusee");
+gsap.to(chapitre2, {
+  scrollTrigger: {
+    pin: true,
+    scrub: true,
+    markers: true,
+    start: "top top",
+    end: "bottom 40%",
+    trigger: chapitre6,
+  },
 });
 
 //animation timeline des planetes
